1b781453577f0de5d1471c0d5f023cadab56aa85
kerber <robertokerber@gmail.com>
2023-09-28 12:07:08 +0100

    Initial commit

---
+++ src/main/java/br/com/consorcio/ConsorcioApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.consorcio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsorcioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsorcioApiApplication.class, args);
+	}
+
+}
