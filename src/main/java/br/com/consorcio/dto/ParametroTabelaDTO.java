632a19bf323c70db3a4ac979897d22873d608f73
kerber <robertokerber@gmail.com>
2023-09-28 12:10:24 +0100

    feature/initial-commit

---
+++ src/main/java/br/com/consorcio/dto/ParametroTabelaDTO.java
@@ -0,0 +1,16 @@
+package br.com.consorcio.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ParametroTabelaDTO {
+
+    private String incc;
+
+}
