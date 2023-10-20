c0eeb46ca47bcd05d3b7db07537d15b7d5261be7
kerber <robertokerber@gmail.com>
2023-10-20 12:03:59 +0100

    Refatoracao do Codigo

--- src/main/java/br/com/consorcio/resource/SimulacaoResource.java
+++ src/main/java/br/com/consorcio/resource/SimulacaoResource.java
@@ -1,10 +1,16 @@
 package br.com.consorcio.resource;
 
 import br.com.consorcio.dto.ParametroRequestDTO;
+import br.com.consorcio.dto.SimulacaoDTO;
 import br.com.consorcio.service.SimulacaoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 import static org.springframework.http.ResponseEntity.ok;
 
@@ -16,7 +22,7 @@ public class SimulacaoResource {
     private final SimulacaoService simulacaoService;
 
     @PostMapping
-    public ResponseEntity simular(@RequestBody ParametroRequestDTO parametroRequestDTO){
+    public ResponseEntity<List<SimulacaoDTO>> simular(@RequestBody ParametroRequestDTO parametroRequestDTO){
         return ok(simulacaoService.simular(parametroRequestDTO));
     }
 
