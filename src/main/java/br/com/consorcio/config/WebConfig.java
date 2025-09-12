738b9f33529ca3ceb89ef74f467e62c1662309d7
kerber <roberto.oliveira-kerber@ext.ec.europa.eu>
2025-09-12 19:08:41 +0200

    Dockerfile criado

--- src/main/java/br/com/consorcio/config/WebConfig.java
+++ src/main/java/br/com/consorcio/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200","https://8386a0254e63.ngrok-free.app/"
+                .allowedOrigins("http://localhost:4200", "http://localhost:8081"
                         ,"https://consorcio-front.sp1.br.saveincloud.net.br")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
